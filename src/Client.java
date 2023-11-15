6725c0bbebcd48a0f122bf6e0605acf47e99d121
Yousef Bakhsh <yousef.bakhsh@gmail.com>
2023-11-15 15:50:00 +0300

    Fixed Client and Server

--- src/Client.java
+++ src/Client.java
@@ -3,89 +3,93 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args){
-        Socket socket = null;
-        InputStreamReader input = null; // receive data from server
-        OutputStreamWriter output = null; // send data to server
+    static Socket socket; // socket class uses TCP/IP
+    static InputStreamReader input; // receive data from server
+    static OutputStreamWriter output; // send data to server
 
-        // use buffer to speed up operation
+    // use buffer to speed up operation
 
-        BufferedReader bufferedReader = null;
-        BufferedWriter bufferedWriter = null;
+    static BufferedReader bufferedReader;
+    static BufferedWriter bufferedWriter;
+
+    public static void main(String[] args) throws IOException {
 
         // create socket
+        createSocket();
+        Scanner userInput = new Scanner(System.in); // user input
+
+        while (true) {
+
+            // get user input
+            // if user enters a string, only first character will be used
+            System.out.println("Enter a Character to be searched: "); 
+            char character = userInput.nextLine().charAt(0);
+            System.out.println("Enter string: ");
+            String string = userInput.nextLine();
+
+            // send messages to server
+            sendUserInput(string, character);
+
+            // server reply
+
+            String serverReply = bufferedReader.readLine();
+            System.out.println("Server: " + serverReply); // print server reply
+
+            System.out.println("Would you like to repeat? (y/n)");
+            char userChoice = userInput.nextLine().charAt(0);
+
+            while (true) {
+                if (userChoice == 'y' || userChoice == 'n')
+                    break;
+                System.out.println("Incorrect input (y/n)");
+                userChoice = userInput.nextLine().charAt(0);
+            }
+
+            // send user choice to server
+            bufferedWriter.write(userChoice);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+
+            // if user choice is no, break
+            if (userChoice == 'n')
+                break;
+
+        }
+        userInput.close(); // close Scanner
+
+    }
+
+    private static void createSocket() {
         try {
             socket = new Socket("localhost", 9000); // used to connect to the server
             input = new InputStreamReader(socket.getInputStream());
             output = new OutputStreamWriter(socket.getOutputStream());
-
             bufferedReader = new BufferedReader(input);
             bufferedWriter = new BufferedWriter(output);
-
-            Scanner userInput = new Scanner(System.in); // user input
-            char character;
-            String string;
-            char userChoice;
-
-            while(true){
-
-                // get user input
-
-                System.out.println("Enter a Character to be searched: ");
-                character = userInput.nextLine().charAt(0);
-                System.out.println("Enter string: ");
-                string = userInput.nextLine();
-
-                // send messages to server
-
-                bufferedWriter.write(string); // send string first
-                bufferedWriter.newLine(); // send new line character
-                bufferedWriter.write(character); // then send character
-                bufferedWriter.newLine(); // send new line character
-                bufferedWriter.flush(); // flush stream 
-
-                // server reply
-
-                String serverReply = bufferedReader.readLine(); 
-                System.out.println(serverReply); // print server reply
-
-                System.out.println("Would you like to repeat?");
-                userChoice = userInput.nextLine().charAt(0);
-
-                if(userChoice == 'n')
-                    break;
-            }
-            userInput.close();
-        }
-        catch(UnknownHostException e){
+        } catch (UnknownHostException e) {
             System.err.println(e);
             System.err.println("Port number provided is Incorrect");
-        }
-        catch(ConnectException e){
+        } catch (ConnectException e) {
             System.err.println(e);
             System.err.println("Server is not running");
+            System.exit(0);
+        } catch (IOException e) {
+            System.err.println(e);
         }
-        catch(IOException e){
-            e.printStackTrace();
+        
+    }
+
+    private static void sendUserInput(String string, char character) throws IOException {
+        try {
+            bufferedWriter.write(string); // send string first
+            bufferedWriter.newLine(); // send new line character
+            bufferedWriter.write(character); // then send character
+            bufferedWriter.newLine(); // send new line character
+            bufferedWriter.flush(); // flush stream
+        } catch (SocketException e) {
+            System.err.println("Connection to Server Lost");
+            System.exit(0);
         }
-    
-        finally{
-            try{
-                if(socket != null)
-                    socket.close();
-                if(input != null)
-                    input.close();
-                if(output != null)
-                    output.close();
-                if(bufferedReader != null)
-                    bufferedReader.close();
-                if(bufferedWriter != null)
-                    bufferedWriter.close();
-                
-            }
-            catch(IOException e){
-                e.printStackTrace();
-            }
-        }
+
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,59 +1,95 @@
 import java.net.*;
 import java.io.*;
 
-
 public class Server {
-    public static void main(String[] args) throws IOException{
-        // a little bit similar to the client
+    static Socket socket; // socket class uses TCP/IP
+    static InputStreamReader input; // receive data from server
+    static OutputStreamWriter output; // send data to server
 
-        Socket socket = null;
-        InputStreamReader input = null; // receive data from server
-        OutputStreamWriter output = null; // send data to server
+    // use buffer to speed up operation
+
+    static BufferedReader bufferedReader;
+    static BufferedWriter bufferedWriter;
+
+    public static void main(String[] args) throws IOException {
+
         ServerSocket serverSocket = new ServerSocket(9000); // waits for requests on a certain port number (9000)
 
+        while (true) {
+            initServer(serverSocket);
+            // second while loop to keep serving client
 
-        // use buffer to speed up operation
-
-        BufferedReader bufferedReader = null;
-        BufferedWriter bufferedWriter = null;
-
-
-        while(true){
-            try{
-                socket = serverSocket.accept();
-                input = new InputStreamReader(socket.getInputStream());
-                output = new OutputStreamWriter(socket.getOutputStream());
-
-                bufferedReader = new BufferedReader(input);
-                bufferedWriter = new BufferedWriter(output);
-
-
-                // second while loop to keep serving client 
-                while(true){
+            while (true) {
+                try {
+                    // receive input fr om user
                     String string = bufferedReader.readLine();
                     char character = bufferedReader.readLine().charAt(0);
-                    
-                    System.out.println("Client: " + string + character);
 
-                    bufferedWriter.write("Message received");
+                    System.out.print("String: " + string + ", Character: " + character + "\n"); // print inputs on
+                                                                                                // server side
+
+                    int characterCount = countCharacters(string, character); // get character count
+                    bufferedWriter.write("Number of Occurrences: " + characterCount); // send character count
                     bufferedWriter.newLine();
-                    bufferedWriter.write(character + " " + string);
                     bufferedWriter.flush();
 
-                    break;
+                    char userChoice = bufferedReader.readLine().charAt(0); // receive user choice
+                    if (userChoice == 'n') { // if user choice == n, break and close server
+                        System.out.println("Client Disconnected");
+                        break;
+                    }
+                } catch (SocketException e) {
+                    System.err.println("Connection to Client Lost");
+                    System.exit(0);
                 }
 
-                socket.close();
-                input.close();
-                output.close();
-
-
             }
-            catch(IOException e){
-                e.printStackTrace();
-            }
+
+            closeServer(serverSocket);
+            break;
+
         }
 
+    }
 
+    private static void initServer(ServerSocket serverSocket) {
+        try {
+            System.out.println("Server is Up and Running");
+            socket = serverSocket.accept();
+            System.out.println("Client Connected!");
+            input = new InputStreamReader(socket.getInputStream());
+            output = new OutputStreamWriter(socket.getOutputStream());
+            bufferedReader = new BufferedReader(input);
+            bufferedWriter = new BufferedWriter(output);
+        } catch (SocketException e) {
+            System.err.println("Connection to Client Lost");
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+
+    }
+
+    private static void closeServer(ServerSocket serverSocket) {
+        try {
+            socket.close();
+            input.close();
+            output.close();
+            bufferedReader.close();
+            bufferedWriter.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+
+    }
+
+    private static int countCharacters(String string, char character) {
+        int count = 0;
+
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == character)
+                count++;
+        }
+        return count;
     }
 }
